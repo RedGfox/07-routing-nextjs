4e6d9cd8129773d8924a4df6c5791794bd375072
RedGfox <zim238kosmit@gmail.com>
2025-08-09 18:27:55 +0300

    fixPromise

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -2,13 +2,12 @@ import NotesClient from './Notes.client';
 import { fetchNotes, FetchNoteResponse } from '@/lib/api';
 
 interface PageProps {
-  params: { slug?: string[] };
+  params: Promise<{ slug?: string[] }>;
 }
 
 export default async function NotesPage({ params }: PageProps) {
-  const slugArray = (await params).slug || [];
-  const tag = slugArray[0] ?? 'All';
-
+  const { slug = [] } = await params;
+  const tag = slug[0] ?? 'All';
   const fetchTag = tag === 'All' ? 'All' : tag;
 
   const initialData: FetchNoteResponse = await fetchNotes(fetchTag, '', 1);
